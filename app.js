c68c760a2c3bcb7f4f7146f5fb8802f5faa8a16a
Igor <igor.agf@mail.ru>
2021-12-22 22:15:52 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+ let incr = 10,
+     decr = 10;
+
+//постнытские
+ incr++; // оператор инкремента
+ decr--; // оператор дикримента
+//префексные
+ ++incr; // оператор инкремента
+ --decr; // оператор дикримента
+ 
+ console.log(incr++); // Вначале выводит потом выполняет операцию инкремента
+ console.log(incr);
+ console.log(decr);
+
+ console.log(5%2);
+ console.log(2*4=='8');
+ console.log(2*4==='8');// Строгое значение
+
+ const isChecked = true,
+        isCose = false;
+
+console.log(isChecked&&isCose); // опартор И
+console.log(isChecked||isCose); // опартор ИЛИ
+console.log(isChecked&& !isCose); // опартор ! отрицания
+// 
+
+// порядок выполнения операции
+
+console.log(2+2*2!=='6'); // 
+
+
+ 
+
+
